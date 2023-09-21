fe6b97ff42db47f47f586756c82d3647982bad43
JH.Zhang <128407894+Mudman14@users.noreply.github.com>
2023-09-21 13:40:28 +0800

    Add files via upload
    finding_motif_on_dna

---
+++ find_motif.py
@@ -0,0 +1,14 @@
+t = 'ATAT' #patern string
+s = '' #parent string
+import re 
+def find_motif(t, s):
+    locations = [] 
+    for match in re.finditer('(?={})'.format(t), s):
+        locations.append(match.start() + 1)
+    return locations 
+
+output = find_motif(t, s)
+print(output)
+
+
+        
